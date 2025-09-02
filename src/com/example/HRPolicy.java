fe7b4f0b7977ebc561b816615adfa574d826202e
AmirReaper <amirrep98@gmail.com>
2025-09-02 23:07:38 +0330

    feat: add HRPolicy final class with salary/hourly rate constants

---
+++ src/com/example/HRPolicy.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Final class for HR constants.
+ */
+public final class HRPolicy {
+    public static final int MIN_SALARY = 1000;
+    public static final int MAX_SALARY = 100000;
+    public static final int MIN_HOURLY_RATE = 10;
+    public static final int MAX_HOURLY_RATE = 1000;
+
+    private HRPolicy() {
+        // prevent instantiation
+    }
+}
